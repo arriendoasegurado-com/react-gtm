90ea269de95ebd4766f6c3b76bebb5320c9a5e78
jupardo <jd.pardo@uniandes.edu.co>
2021-02-22 21:09:31 -0500

    feat/workerSupport: add worker support

--- src/TagManager.js
+++ src/TagManager.js
@@ -2,7 +2,7 @@ import Snippets from './Snippets'
 
 const TagManager = {
   dataScript: function (dataLayer) {
-    const script = document.createElement('script')
+    const script = self.document.createElement('script')
     script.innerHTML = dataLayer
     return script
   },
@@ -10,13 +10,13 @@ const TagManager = {
     const snippets = Snippets.tags(args)
 
     const noScript = () => {
-      const noscript = document.createElement('noscript')
+      const noscript = self.document.createElement('noscript')
       noscript.innerHTML = snippets.iframe
       return noscript
     }
 
     const script = () => {
-      const script = document.createElement('script')
+      const script = self.document.createElement('script')
       script.innerHTML = snippets.script
       return script
     }
@@ -38,15 +38,15 @@ const TagManager = {
       auth,
       preview
     })
-    if (dataLayer) document.head.appendChild(gtm.dataScript)
-    document.head.insertBefore(gtm.script(), document.head.childNodes[0])
-    document.body.insertBefore(gtm.noScript(), document.body.childNodes[0])
+    if (dataLayer) self.document.head.appendChild(gtm.dataScript)
+    self.document.head.insertBefore(gtm.script(), self.document.head.childNodes[0])
+    self.document.body.insertBefore(gtm.noScript(), self.document.body.childNodes[0])
   },
   dataLayer: function ({dataLayer, dataLayerName = 'dataLayer'}) {
     if (window[dataLayerName]) return window[dataLayerName].push(dataLayer)
     const snippets = Snippets.dataLayer(dataLayer, dataLayerName)
     const dataScript = this.dataScript(snippets)
-    document.head.insertBefore(dataScript, document.head.childNodes[0])
+    self.document.head.insertBefore(dataScript, self.document.head.childNodes[0])
   }
 }
 
